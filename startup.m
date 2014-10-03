db891c4554fd5ee6f01df3131adc96b5daa167a5
Avinash Pujala <avinashpujala@gmail.com>
2014-10-03 11:54:02 -0400

    Moved folders

---
+++ startup.m
@@ -0,0 +1,12 @@
+%  C:\Users\pujalaa\Documents\MATLAB\prt
+%  prtPath;
+
+% path(path, genpath('\\dm11\koyamalab\Code\mfiles\nit'));
+addpath(genpath('\\dm11\koyamalab\Code'));
+addpath(genpath('c:\Users\pujalaa\Documents\MATLAB'));
+addpath(genpath('E:\Code'));
+addpath('C:\Users\pujalaa\Documents\MATLAB\SPIM_cell_view');
+addpath('C:\Users\pujalaa\Documents\MATLAB\SPIM_cell_view\functions');
+javaaddpath('C:\Program Files\ImageJ\ij.jar');
+addpath(genpath('C:\Users\pujalaa\Documents\MATLAB\SPIM_cell_view'));
+% addpath(genpath('C:\Users\pujalaa\Documents\MATLAB\correlCorresp'));
