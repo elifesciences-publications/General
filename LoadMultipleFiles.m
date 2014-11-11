50cc876a728bba924355be985650cb84fddd38d3
Avinash Pujala <avinashpujala@gmail.com>
2014-11-11 14:52:04 -0500

    Displays that an error occurred when executing catch block of try...catch...end

--- LoadMultipleFiles.m
+++ LoadMultipleFiles.m
@@ -14,12 +14,12 @@ try
     [dataStruct,timeAxisStruct,samplingInts] = ...
         createdatastructure(files,paths);
 catch
-   
+fprintf('\n Automatic file detection failed, please choose files manually! \n')
 sid = getsid;
 if strcmpi(sid,'S-1-5-21-3197369867-541179473-1092110829')
 cd('C:\Users\Avi\Documents\All Things Research\Research Data\Data');    
 end
-
+clear files paths
 [files,paths] = uigetfile({'*.*';'*.abf';'*.atf';'*.mat';},'SELECT MULTIPLE FILES USING CTRL OR SHIFT KEY','Multiselect','on');
 nFiles = size(char(files),1);
 [dataStruct,timeAxisStruct,samplingInts] = createdatastructure(files,paths);
