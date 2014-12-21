7bebf704eb7864dc69eb2a77209590ba128b76d7
Avinash Pujala <avinashpujala@gmail.com>
2014-12-20 19:40:17 -0500

    New code for deartifaction with untranslated code, existing code modified to use a narrower Gaussian in 2nd iteration

---
+++ EPhys/DeartifactWithUntranslatedGauss.m
@@ -0,0 +1,68 @@
+
+
+function artlessSignal = DeartifactWithUntranslatedGauss(signal,samplingInt, stimInds,preStimPeriod, postStimPeriod)
+
+% DeartifactWithUntranslatedGauss - Remove stimulus artifacts. In contrast to
+%                      Deartifact, subtracts Gaussian-modulated artifact for selected time
+%                       period and also adds some Gaussian-modulated noise
+%   data_mod = DeartifactWithUntranslatedGauss(data,samplingInt,stimIndices,preStimPeriod, postStimPeriod); 
+%   data - signal vector
+%   timeAxis - time vector
+%   preStimPeriod - time period before stimulus in which artifact is
+%                   considered to have influence (due to filtering, etc)
+%   postStimPeriod - time period after stimulus in which artifact is
+%                     considered to have influence
+ 
+if size(signal,1) < size(signal,2)
+    signal = signal';
+end
+
+preStimPts = round(preStimPeriod/samplingInt);
+postStimPts = round(postStimPeriod/samplingInt);
+alpha = 2; % The larger the value, the broader the gaussian kernel 
+
+artlessSignal = signal;
+noiseLevel = 0.1*std(artlessSignal,[],1);
+
+for stim = 1:numel(stimInds)
+    %%%%%%%%% First iteration %%%%%%%%%%
+    artifact =[];
+    fpt = round(max(stimInds(stim)-preStimPts,1));
+    lpt = round(min(stimInds(stim) + postStimPts, length(signal)));
+    noise = repmat(noiseLevel,numel(fpt:lpt),1);
+   noise = noise.*randn(size(noise));
+    artifact = signal(fpt:lpt,:);
+    lenArt = length(fpt:lpt);    
+    kernel = gausswin(lenArt,alpha);     
+     kernel = (kernel-min(kernel))/(max(kernel)-min(kernel));    
+    kernel =  repmat(kernel(:),1, size(signal,2));
+     artifact = (artifact.*kernel);
+
+%     artlessSignal(fpt:lpt,:) = signal(fpt:lpt,:)-artifact + slopedLine; % Same as setting artifact to zero
+      artlessSignal(fpt:lpt,:) = signal(fpt:lpt,:)-artifact; + noise;
+% %       plot(artlessSignal(fpt:lpt,:),'k')
+
+
+
+     
+%  %%%%%%%%%%%%%%%% 2nd iteration %%%%%%%%%%%%%%%%%%
+      artifact = artlessSignal(fpt:lpt,:);
+      artifact = (artifact.*kernel);
+       artlessSignal(fpt:lpt,:) = signal(fpt:lpt,:)-artifact; + noise;
+
+%       kernel = gausswin(3*lenArt,3*alpha); % Making kernel longer than signal to accommodate for circhift coming up
+%       [~, kerMaxInd] = max(kernel,[],1);  
+%      [~,sigMaxInd] = max(abs(artifact),[],1);
+%     kernel2 = circshift(kernel2(:),sigMaxInd-kerMaxInd); % Shifting to align peak of kernel with peak of artifact
+%     kernel2([1:lenArt 2*lenArt+1:end])=[]; % Taking the central segment of kernel2 that matches signal in length
+%     kernel2 = (kernel2-min(kernel2))/(max(kernel2)-min(kernel2)); 
+% %     plot(kernel2*max(artifact),'c')
+%     kernel2 =  repmat(kernel2(:),1,size(signal,2)); 
+%      noise = noise.*kernel2;
+%      artifact = (artifact.*kernel2);
+% %      plot(artifact,'r:')
+%       artlessSignal(fpt:lpt,:) = artlessSignal(fpt:lpt,:)-artifact + noise;
+% %      plot(artlessSignal(fpt:lpt,:),'k'), 
+       
+end
+
--- scratch.m
+++ scratch.m
@@ -1,11 +1,7 @@
-%% Smoothing and segmenting data by trials
-
 artlessSignal = zeros(size(dataChannels));
 smSignal = artlessSignal;
-for cn = 1:numel(ePhysChannelToAnalyze); 
-     artlessSignal(cn,:) = DeartifactWithGauss(dataChannels(cn,:),samplingInt,artInds,20e-3,20e-3); % 45e-3 seems to work best!
-    artlessSignal(cn,:) = DeartifactWithGaussInterp(artlessSignal(cn,:),samplingInt,artInds,20e-3,20e-3); % 45e-3 seems to work best!
-    
+for cn = 1:numel(ePhysChannelToAnalyze);    
+    artlessSignal(cn,:) = DeartifactWithGauss(dataChannels(cn,:),samplingInt,artInds,20e-3,40e-3);
     artlessSignal(cn,:) = chebfilt(artlessSignal(cn,:),samplingInt,[hpFilt 1000]); 
     blah = artlessSignal(cn,:);
     threshLevel = mean(blah) + 10*std(blah);
@@ -13,7 +9,7 @@ for cn = 1:numel(ePhysChannelToAnalyze);
     blah(blah < -threshLevel) = -threshLevel; % Saturating at value of 10
     artlessSignal(cn,:) = blah;
     smSignal(cn,:) = SmoothVRSignals(artlessSignal(cn,:),samplingInt,hpFilt,lpFilt);
-    smSignal(cn,:) = DeartifactWithGaussInterp(smSignal(cn,:),samplingInt,stimInds,20e-3,30e-3);
+    smSignal(cn,:) = DeartifactWithGauss(smSignal(cn,:),samplingInt,stimInds,20e-3,30e-3);
     blah = smSignal(cn,:);
     threshLevel = mean(blah) + 50*std(blah);
      blah(blah > threshLevel)= threshLevel;
@@ -25,9 +21,3 @@ preStimPts  = round(preStimPeriod*samplingRate);
 postStimPts = round(postStimPeriod*samplingRate);
 
 fprintf('\n Deartifacting & smoothing signals \n')
-
-% artlessSignal = BlankArtifact(artlessSignal,samplingInt,stimInds,5e-3, 10e-3);
-% 
-% smSignal = SmoothVRSignals(artlessSignal,samplingInt,hpFilt,lpFilt);
-
-% smDiffSignal= SmoothDiffVRSignals(artlessSignal,samplingInt,hpFilt,lpFilt);
