2e3027db3e43c69f3d7cad9295f181cd6bd1aa45
Avinash Pujala <avinashpujala@gmail.com>
2015-02-02 17:57:48 -0500

    SubtractMinimaEnvelope: Function for obtaining Minimal Envelope & subtracting this from the original signal

---
+++ SubtractMinimalEnvelope.m
@@ -0,0 +1,21 @@
+function varargout = SubtractMinimalEnvelope(x)
+%SubtractMinimalEnvelope- Finds the minima in a signal, cubic interpolates
+%                    between these points and subtracts this from the
+%                    original signal
+% 
+% y = SubtractMinimalEnvelope(x);
+% [y,minimaEnvelope,minimaInds] = SubtractMinimalEnvelope(x);
+% Outputs:
+% minimaEnvelope - the interpolated minima envelope
+% minimaInds - indices of the minima found in the original signal
+
+minimaInds = findpeaks_hht(-x);
+x = x(:)';
+s = interp1([0 minimaInds(:)' length(x)+1],[x(1) x(minimaInds(:)) x(end)],1:length(x),'cubic');
+y = x - s;
+
+varargout{1} = y;
+varargout{2} = s;
+varargout{3} = minimaInds;
+end
+
