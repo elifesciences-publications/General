d147a627457114533e0afca74eba5544bb0c80e6
Avinash Pujala <avinashpujala@gmail.com>
2015-03-12 21:51:09 -0400

    FindPeaksByLevel: A simple function for finding peaks that exceed a certain level/amplitude

---
+++ FindPeaksByLevel.m
@@ -0,0 +1,16 @@
+function varargout = FindPeaksByLevel(x,level)
+% FindPeaksByLevel - FindPeaks within a signal that exceed a certain level
+% 
+% [pks,pkInds] = FindPeaksByLevel;
+
+pkInds = findpeaks_hht(x);
+pks = x(pkInds);
+
+pkInds( pks < level) = [];
+pks(pks < level) = [];
+
+varargout{1} = pks;
+varargout{2} = pkInds;
+
+end
+
