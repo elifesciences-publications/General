efd9d7f5c4e4fa095dfa61b357bad66dfb577dda
Avinash Pujala <avinashpujala@gmail.com>
2015-11-25 15:17:21 -0500

    Updated before pulling

--- scratch.m
+++ scratch.m
@@ -1,114 +1,28 @@
-
-function WriteLSVideo(aveResponseImg, baselineImg, dff5secImg, nRows, nCols, movName,output_dir)
-
-  %% Videowriter
-   stackDim = size(aveResponseImg);
-    currentPath = cd;
-    cd(output_dir)
-    monSize = getMonitorSize;
-   
-    figure('units','pixels','outerposition',[0 0 monSize(3)-0.1*monSize(3) monSize(4)-0.1*monSize(4)]);    
-    writerObj = VideoWriter(movName,'Uncompressed AVI');
-    writerObj.FrameRate = 5;
-    open(writerObj);    
-    for i = 1:size(aveResponseImg,4)
-        f1 = (aveResponseImg(:,:,:,i)./baselineImg)-1; 
-        gcf;
-        h = imagesc(reshape(permute(reshape(f1,[stackDim(1) stackDim(2) nRows nCols]), [1 4 2 3]), ...
-            [stackDim(1)*nCols, stackDim(2)*nRows]),[-0.2 0.5]);
-        
-        axis off;
-        axis image;
-        title(['frame no: ', int2str(i)]);
-        colorbar;
-        colormap gray;
-        set(gca,'Units','normalized','Position',[0.025 0.025 0.95 0.95]);
-        %     F(i) = getframe(gcf);
-        frame = getframe(gcf);
-        writeVideo(writerObj,frame);
-    end
-    
-    % movie(F,20);
-    close(writerObj);
-    
-     %% dff
-    fprintf('\n Creating dff stack \n')
-    dff = zeros(stackDim(1)*nCols, stackDim(2)*nRows,size(aveResponseImg,4));
-    for i = 1:size(aveResponseImg,4)
-        f1 = (aveResponseImg(:,:,:,i)./baselineImg)-1;
-        dff(:,:,i) = reshape(permute(reshape(f1,[stackDim(1) stackDim(2) nRows nCols]), [1 4 2 3]), [stackDim(1)*nCols, stackDim(2)*nRows]);
-    end
-    fh = fopen(['dff_' movName '.bin'],'w');
-    fwrite(fh, dff,'double');
-    fclose(fh);
-    fprintf('\n Done! \n')
-    
-    %% stflat
-    fprintf('\n Creating stflat stack \n')
-    stflat = zeros(stackDim(1)*nCols, stackDim(2)*nRows,size(aveResponseImg,4));
-    for i = 1:size(aveResponseImg,4)
-        stflat(:,:,i) = reshape(permute(reshape((baselineImg-min(baselineImg(:)))/max(baselineImg(:)),[stackDim(1) stackDim(2) nRows nCols]), [1 4 2 3]), [stackDim(1)*nCols, stackDim(2)*nRows]);
-    end
-    fh = fopen(['stflat_' movName '.bin'],'w');
-    fwrite(fh, stflat,'double');
-    fclose(fh);
-    fprintf('\n Done! \n')
-    
-    %%
-    dffPlus= zeros(stackDim);dffNegative = zeros(stackDim);
-    plusIndex = dff5secImg(:)>0.02;
-    negIndex = dff5secImg(:)<-0.02;
-    dffPlus(plusIndex) = dff5secImg(plusIndex);
-    dffNegative(negIndex) = -dff5secImg(negIndex);
-    
-    nor_dffPlus = dffPlus/0.5;
-    nor_dffNegative = dffNegative/0.5;
-    
-    figure('position', [0 0 1600*stackDim(2)/stackDim(1) 1600]);
-    imagesc(baselineImg(:,:,24));
-    axis image;
-    axis off;
-    set(gca,'Units','normalized', 'Position', [0 0 1 1]);
-    
-    movName = [fName '_' timeStamp '_ResponseMap.avi'];
-    writerObj = VideoWriter(movName,'Uncompressed AVI');
-    writerObj.FrameRate = 1;
-    open(writerObj);
-    stImg = (baselineImg-min(baselineImg(:)))/max(baselineImg(:));
-    for i = 1:size(baselineImg,3)
-        imagesc(stImg(:,:,i),[0 0.5]);
-        colormap(gray);
-        axis image;
-        axis off;
-        set(gca,'Units','normalized','Position',[0 0 1 1]);
-        hold on;
-        
-        red = cat(3, ones(stackDim(1:2)),zeros(stackDim(1:2)),zeros(stackDim(1:2)));
-        green = cat(3, zeros(stackDim(1:2)),ones(stackDim(1:2)),zeros(stackDim(1:2)));
-        
-        h_red = imshow(red);
-        set(h_red,'AlphaData',nor_dffPlus(:,:,i));
-        
-        h_green = imshow(green);
-        set(h_green,'AlphaData',nor_dffNegative(:,:,i));
-        hold off;
-        
-        frame = getframe;
-        writeVideo(writerObj,frame);
-    end
-    
-    close(writerObj);
-    
-    cd(currentPath) 
-     
-    
-    
-    
+%% Converting raw cells traces to dF/F & filtering a bit
+nBaselineFrames = round(min(10,data.baselinePeriod)/stack.interval);
+baselineInds = [];
+for shock = 1:numel(stack.inds.shock)
+    baselineInds = [baselineInds, stack.inds.shock(shock)-nBaselineFrames:stack.inds.shock(shock)-1];
 end
-    
-    
-    
-    
-    
+baselineInds(baselineInds <=0) = [];
+baselineF = repmat(mean(data.cells.raw(:,baselineInds),2),1,size(data.cells.raw,2));
+data.cells.dFF = (data.cells.raw - baselineF)./baselineF;
 
+nyqFreq = 0.5*(1/stack.interval);
+lpf = 0.9*nyqFreq;
+kernelWidth = max(round(1/lpf),5); 
+kernel = gausswin(kernelWidth); % Not using at the moment
+kernel = kernel/sum(kernel);
 
+lastPt = min(stack.N,size(data.cells.raw,2));
+nCells = size(data.cells.raw,1);
+data.cells.time = (0:size(data.cells.raw,2)-1)*stack.interval;
+
+disp(['Smoothing cellular Ca++ responses with lowpass of  ' num2str(lpf)])
+if strcmpi(smoothingType,'conv')
+    data.cells.dFF = chebfilt(conv2(double(data.cells.dFF'),kernel(:)','same'),stack.interval,1/200,'high');
+    data.cells.dFF = data.cells.dFF';
+else
+    data.cells.dFF = chebfilt(chebfilt(double(data.cells.dFF'),stack.interval,lpf,'low'),stack.interval,1/200,'high');
+    data.cells.dFF = data.cells.dFF';
+end
