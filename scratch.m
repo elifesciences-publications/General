0308ed3f23c750b20775e74254d8ed9192092d51
Avinash Pujala <avinashpujala@gmail.com>
2016-02-23 16:50:19 -0500

    Downloaded patchline.m

--- scratch.m
+++ scratch.m
@@ -1,31 +1,50 @@
 
-% posMap = (posMap-min(posMap(:)))/(max(posMap(:))-min(posMap(:)));
-% negMap = (negMap-min(negMap(:)))/(max(negMap(:))-min(negMap(:)));
-% avgStack = (stack.avg-min(stack.avg(:)))/(max(stack.avg(:))-min(stack.avg(:)));
-% activityMap = permute(cat(4,posMap, negMap, 0.25*avgStack),[2 1 4 3]);
+%% Estimating turns with respect to prev swim direction vectors
+turnAngleThr = 5;
 
+clear i
+tracexy_motion = tracexy(motionFrames,:);
+motionVecs = diff(tracexy_motion);
+motionVecs = motionVecs(:,1) + motionVecs(:,2)*i;
+dTh = angle(motionVecs(1:end-1).*conj(motionVecs(2:end)));
+dTh_motion = [0; dTh(:)]*180/pi;
+dS_motion = abs(motionVecs);
 
-function SaveImageStack(I, imgDir, fileName)
-if ~exist(imgDir)
-    mkdir(imgDir)
-end
+dS_rightInds = find(dTh_motion < -turnAngleThr);
+dS_leftInds = find(dTh_motion > turnAngleThr);
+dS_straightInds = find(abs(dTh_motion) <= turnAngleThr);
 
-if ndims(I) == 4
-    nImages = size(I,4);
-elseif ndims(I) == 3
-    nImages = size(I,3);
-elseif nDims(I)==2
-    nImages = 1
-else
-    disp('Check size of image input!')
-end
+dS_left = dS_motion(dS_leftInds);
+dS_right = dS_motion(dS_rightInds);
+dS_straight = dS_motion(dS_straightInds);
 
-fp = fullfile(imgDir,[fileName '.tif']);
-for imgNum = 1:nImages
-    if ndims(I)==4
-        imwrite(I(:,:,:,imgNum),fp,'tif')
-    else
-        imwrire(I(:,:,imgNum),fp,'tif')
-    end
-    disp(imgNum)
-end
+[dS_left_count, dS_left_vals] = hist(dS_left,50);
+[dS_right_count, dS_right_vals] = hist(dS_right,50);
+[dS_straight_count, dS_straight_vals] = hist(dS_straight);
+
+ker = gausswin(3); ker = ker/sum(ker);
+dS_left_count = conv2(dS_left_count(:),ker(:),'same');
+dS_left_count = [0; dS_left_count(:)];
+dS_left_vals = [0; dS_left_vals(:)];
+
+dS_right_count = conv2(dS_right_count(:),ker(:),'same');
+dS_right_count = [0; dS_right_count(:)];
+dS_right_vals = [0; dS_right_vals(:)];
+
+dS_straight_count = conv2(dS_straight_count(:),ker(:),'same');
+dS_straight_count = [0; dS_straight_count(:)];
+dS_straight_vals = [0; dS_straight_vals(:)];
+
+%% Some Plotting
+figure('Name','Distribution of swim distances between moving frames sorted by turn direction')
+plot(dS_left_vals,dS_left_count,'linewidth',2)
+hold on
+plot(dS_right_vals,dS_right_count,'r','linewidth',2)
+plot(dS_straight_vals,dS_straight_count,'k','linewidth',2)
+legend('Left','Right','Straight')
+xlim([0 40])
+ylim([-inf inf])
+xlabel('Swim distance between moving frames')
+ylabel('Count')
+box off
+title('Distribution of swim distances between subsequent moving frames sorted by turn direction')
