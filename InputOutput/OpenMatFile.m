be86388082b2f0fc0c12c0484798db9200fe2928
Avinash Pujala <avinashpujala@gmail.com>
2016-08-11 17:40:12 -0400

    A simple function for creating a variable in the workspace that points to a matfile that is interactively chosen

---
+++ InputOutput/OpenMatFile.m
@@ -0,0 +1,25 @@
+function varargout = OpenMatFile(varargin)
+%OpenMatFile Interactively allows opening of matfile and creation of a
+%   variable in the workspace that points to the matfile.
+% var = OpenMatFile();
+% var = OpenMatFile(fullPathToMatFile);
+% 
+% Avinash Pujala, HHMI, 2016
+
+if nargin ==0
+    [file,path] = uigetfile('*.mat');
+    var = matfile(fullfile(path,file));
+elseif nargin ==1
+    if isdir(varargin{1});
+        cd(varargin{1});
+        [file,path] = uigetfile('*.mat');
+        var = matfile(fullfile(path,file));
+    else
+        var = matfile(varargin{1});
+    end
+end
+
+varargout{1} = var;
+
+end
+
